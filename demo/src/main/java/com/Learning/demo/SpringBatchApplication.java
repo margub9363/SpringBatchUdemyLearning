591d1acde48b72929e1eda3013599b1c8996be59
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-09-01 22:42:06 +0530

    Adding Annotation to Enable Batch Processing

--- demo/src/main/java/com/Learning/demo/SpringBatchApplication.java
+++ demo/src/main/java/com/Learning/demo/SpringBatchApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class SpringBatchApplication {
 
 	public static void main(String[] args) {
