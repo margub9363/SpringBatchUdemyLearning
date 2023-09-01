0c16e2e6a482a5be41865dfdc3a6f3707fc266e0
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-09-01 22:41:03 +0530

    Basic Project Setup

---
+++ demo/src/test/java/com/Learning/demo/SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Learning.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
