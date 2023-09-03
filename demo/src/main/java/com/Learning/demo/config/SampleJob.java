9dea608d8a89f403ca670c72cd4782d9078347f2
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-09-03 13:00:03 +0530

    Implemented the Flat File Item Reader - L380

--- demo/src/main/java/com/Learning/demo/config/SampleJob.java
+++ demo/src/main/java/com/Learning/demo/config/SampleJob.java
@@ -1,5 +1,6 @@
 package com.Learning.demo.config;
 
+import com.Learning.demo.model.StudentCsv;
 import com.Learning.demo.processor.FirstItemProcessor;
 import com.Learning.demo.reader.FirstItemReader;
 import com.Learning.demo.writer.FirstItemWriter;
@@ -8,9 +9,17 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.file.transform.LineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+
+import java.io.File;
 
 @Configuration
 public class SampleJob {
@@ -30,8 +39,8 @@ public class SampleJob {
 
     private Step firstChunkStep() {
         return stepBuilderFactory.get("First Chunk Step")
-                .<Integer,Long>chunk(3)
-                .reader(firstItemReader)
+                .<StudentCsv,StudentCsv>chunk(3)
+                .reader(flatFileItemReader())
 //                .processor(firstItemProcessor)
                 .writer(firstItemWriter)
                 .build();
@@ -43,6 +52,31 @@ public class SampleJob {
                 .incrementer(new RunIdIncrementer())
                 .start(firstChunkStep())
                 .build();
+    }
+
+//    @Bean
+    public FlatFileItemReader<StudentCsv> flatFileItemReader(){
+        FlatFileItemReader<StudentCsv> flatFileItemReader = new FlatFileItemReader<StudentCsv>();
+        flatFileItemReader.setResource(new FileSystemResource(
+                new File("D:\\IntellijPracticeProjects\\SpringBatchUdemyLearning\\demo\\InputFiles\\students.csv")
+        ));
+       flatFileItemReader.setLineMapper(new DefaultLineMapper<StudentCsv>(){
+           {
+               setLineTokenizer(new DelimitedLineTokenizer(){
+                   {
+                       setNames("ID","First Name","Last Name","Email" );
+                   }
+               });
+               setFieldSetMapper(new BeanWrapperFieldSetMapper<StudentCsv>(){
+                   {
+                       setTargetType(StudentCsv.class);
+                   }
+               });
+           }
+
+       });
+       flatFileItemReader.setLinesToSkip(1);
+        return flatFileItemReader;
     }
 
 }
--- demo/src/main/java/com/Learning/demo/writer/FirstItemWriter.java
+++ demo/src/main/java/com/Learning/demo/writer/FirstItemWriter.java
@@ -1,15 +1,16 @@
 package com.Learning.demo.writer;
 
+import com.Learning.demo.model.StudentCsv;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
-public class FirstItemWriter implements ItemWriter<Long> {
+public class FirstItemWriter implements ItemWriter<StudentCsv> {
 
     @Override
-    public void write(List<? extends Long> items) throws Exception {
+    public void write(List<? extends StudentCsv> items) throws Exception {
         System.out.println("Inside Item Writer");
         items.stream().forEach(System.out::println);
     }
