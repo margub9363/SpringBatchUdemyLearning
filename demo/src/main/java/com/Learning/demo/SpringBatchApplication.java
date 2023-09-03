96748d5ec8373de0c25979fa3b2bd03c79aaf505
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-09-03 11:56:01 +0530

    Commenting processor to focus more on Reader

--- demo/src/main/java/com/Learning/demo/SpringBatchApplication.java
+++ demo/src/main/java/com/Learning/demo/SpringBatchApplication.java
@@ -8,8 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableBatchProcessing
-@EnableAsync
-//@EnableScheduling
 public class SpringBatchApplication {
 
 	public static void main(String[] args) {
--- demo/src/main/java/com/Learning/demo/config/SampleJob.java
+++ demo/src/main/java/com/Learning/demo/config/SampleJob.java
@@ -1,20 +1,13 @@
 package com.Learning.demo.config;
 
-import com.Learning.demo.listener.FirstJobListener;
-import com.Learning.demo.listener.FirstStepListener;
 import com.Learning.demo.processor.FirstItemProcessor;
 import com.Learning.demo.reader.FirstItemReader;
 import com.Learning.demo.writer.FirstItemWriter;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,68 +20,28 @@ public class SampleJob {
 
     @Autowired
     private StepBuilderFactory stepBuilderFactory;
-
-    @Autowired
-    private Tasklet firstTask;
-
-    @Autowired
-    private Tasklet secondTask;
-
-    @Autowired
-    FirstJobListener firstJobListener;
-
-    @Autowired
-    FirstStepListener firstStepListener;
-
     @Autowired
     private FirstItemReader firstItemReader;
-
     @Autowired
     private FirstItemProcessor firstItemProcessor;
-
     @Autowired
     private FirstItemWriter firstItemWriter;
 
-
-    private Step firstStep() {
-        return stepBuilderFactory.get("First Step")
-                .tasklet(firstTask)
-                .listener(firstStepListener)
-                .build();
-    }
-
-    private Step secondStep() {
-        return stepBuilderFactory.get("Second Step")
-                .tasklet(secondTask)
-                .build();
-    }
-
-
-//    @Bean
-    public Job firstJob() {
-        return jobBuilderFactory.get("First Job")
-                .incrementer(new RunIdIncrementer())
-                .start(firstStep())
-                .next(secondStep())
-                .listener(firstJobListener)
-                .build();
-    }
-
-    @Bean
-    public Job secondJob() {
-        return jobBuilderFactory.get("Second Job")
-                .incrementer(new RunIdIncrementer())
-                .start(firstChunkStep())
-                .next(secondStep())
-                .build();
-    }
 
     private Step firstChunkStep() {
         return stepBuilderFactory.get("First Chunk Step")
                 .<Integer,Long>chunk(3)
                 .reader(firstItemReader)
-                .processor(firstItemProcessor)
+//                .processor(firstItemProcessor)
                 .writer(firstItemWriter)
+                .build();
+    }
+
+    @Bean
+    public Job chunkJob() {
+        return  jobBuilderFactory.get("Chunk Job")
+                .incrementer(new RunIdIncrementer())
+                .start(firstChunkStep())
                 .build();
     }
 
